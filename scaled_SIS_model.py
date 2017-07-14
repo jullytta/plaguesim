79926460b05b3499b41a3eaadd926f8a4280b35d
Jullyana Mattos Vycas <jmattosvycas@uwaterloo.ca>
2017-07-14 18:39:30 -0300

    Function get_configuration added.

--- scaled_SIS_model.py
+++ scaled_SIS_model.py
@@ -13,6 +13,16 @@ def get_clique(N):
   return clique
 
 
+def get_configuration(N, i):
+  x = np.zeros([N, 1], dtype=np.int_)
+
+  for k in range(0, N):
+    infected_flag = (i >> N-(k+1)) & 1
+    x[k] = infected_flag
+
+  return x
+
+
 def main():
   # The population size.
   N = 3
@@ -25,13 +35,17 @@ def main():
   pi = np.zeros([N], dtype=np.float_)
 
   # Current configuration
-  x = np.zeros([N, 1], dtype=np.int_)
+  x = get_configuration(N, 4)
   x_t = np.transpose(x)
 
-  result = np.matmul(A, x)
-  result = np.matmul(x_t, result)
-  print(result)
+  # Transposed vector of ones
+  one_t = np.ones([1, N], dtype=np.int_)
 
+  # Number of infected nodes in this configuration
+  n_infected = np.matmul(one_t, x)[0, 0]
+
+  print("Current configuration: ", x_t)
+  print("# of infected nodes: ", n_infected)
 
 if __name__ == '__main__':
   main()
