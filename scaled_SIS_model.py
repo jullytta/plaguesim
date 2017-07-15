88bdcd4ec3417344aca17d46b94ba12e63d46c7c
Jullyana Mattos Vycas <jmattosvycas@uwaterloo.ca>
2017-07-14 21:09:16 -0300

    Function renaming, comments to make things clear, minor bugfix.

--- scaled_SIS_model.py
+++ scaled_SIS_model.py
@@ -13,7 +13,18 @@ def get_clique(N):
   return clique
 
 
-def get_configuration(N, i):
+################################################################
+# We define a configuration as a state the system can assume.  #
+# Each configuration is a combination of possible values for   #
+# the nodes. A node can be susceptible (0) or infected (1).    #
+# Therefore, a configuration vector is a binary vector of      #
+# size N, number of nodes.                                     #
+#                                                              #
+# Suppose we have N = 3. That means each configuration has     #
+# 3 bits. Configuration 2 equals 010, which means nodes zero   #
+# and two are susceptible, while node one is infected.         #
+################################################################
+def get_configuration_vector(N, i):
   x = np.zeros([N, 1], dtype=np.int_)
 
   for k in range(0, N):
@@ -27,25 +38,42 @@ def main():
   # The population size.
   N = 3
 
+  # The number of different configurations our system
+  # can assume. Each node can be either susceptible (0)
+  # or infected (1), so there are 2^N possible settings.
+  n_configs = pow(N, 2)
+
   # Adjacency matrix
   # We will start working with cliques only
   A = get_clique(N)
 
   # pi[i] is the stationary distribution
-  pi = np.zeros([N], dtype=np.float_)
+  # i.e., the probability of seeing configuration i
+  pi = np.zeros([n_configs], dtype=np.float_)
 
-  # Current configuration
-  x = get_configuration(N, 4)
-  x_t = np.transpose(x)
+  # n_infected[i] is the number of infected nodes
+  # at configuration i
+  n_infected = np.zeros([n_configs], dtype=np.int_)
 
   # Transposed vector of ones
   one_t = np.ones([1, N], dtype=np.int_)
 
-  # Number of infected nodes in this configuration
-  n_infected = np.matmul(one_t, x)[0, 0]
+  # TODO(jullytta): This is where the for loop should start
+  # Current configuration
+  i = 4
 
+  x = get_configuration_vector(N, i)
+  x_t = np.transpose(x)
+
+  # Number of infected nodes in this configuration
+  n_infected[i] = np.matmul(one_t, x)[0, 0]
+
+  # Print stats
+  print("# of nodes: ", N)
+  print("# of configurations: ", n_configs)
   print("Current configuration: ", x_t)
-  print("# of infected nodes: ", n_infected)
+  print("# of infected nodes: ", n_infected[i])
+
 
 if __name__ == '__main__':
   main()
